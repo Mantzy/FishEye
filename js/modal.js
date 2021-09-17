bebeac48ebea9aa1a690787bdde579156ca3d786
Mantzy <fanni.kovacs@hotmail.com>
2021-09-17 14:07:38 +0100

    contact form

---
+++ js/modal.js
@@ -0,0 +1,35 @@
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelectorAll(".close");
+//const modalSuc = document.querySelectorAll(".bgSubmit");
+const modalSucClose = document.getElementById("sucClose");
+const modalSucBtn = document.getElementById("submit-success");
+
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+    modalbg.style.display = "block";
+}
+
+// close modal event
+modalClose.forEach((btn) => btn.addEventListener("click", closeModal));
+
+//close modal form
+function closeModal() {
+    modalbg.style.display = "none";
+
+}
+
+//close submit success page
+modalSucBtn.addEventListener("click", function() {
+    setTimeout(function() { window.location.reload(); }, 2)
+})
+
+modalSucClose.addEventListener("click", function() {
+    setTimeout(function() { window.location.reload(); }, 2)
+})
