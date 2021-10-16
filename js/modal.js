1528a421d2a63727691c0650b446fb0fcbc095ea
Mantzy <fanni.kovacs@hotmail.com>
2021-10-16 18:50:34 +0100

    timout function

--- js/modal.js
+++ js/modal.js
@@ -1,54 +1,61 @@
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const modalClose = document.querySelectorAll(".close");
-//const modalSuc = document.querySelectorAll(".bgSubmit");
+setTimeout(function() {
+
+
+
+
+    // DOM Elements
+    const modalbg = document.querySelector(".bground");
+    const modalBtn = document.querySelectorAll(".modal-btn");
+    const formData = document.querySelectorAll(".formData");
+    const modalClose = document.querySelectorAll(".close");
+    //const modalSuc = document.querySelectorAll(".bgSubmit");
+
+
+
+    // launch modal event
+    modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+    // launch modal form
+    function launchModal() {
+        modalbg.style.display = "block";
+    }
+
+    // close modal event
+    modalClose.forEach((btn) => btn.addEventListener("click", closeModal));
+
+    //close modal form
+    function closeModal() {
+        modalbg.style.display = "none";
+
+    }
+
+    /*modalSucBtn.addEventListener("click", function() {
+        setTimeout(function() { window.location.reload(); }, 2)
+    })
+
+    modalSucClose.addEventListener("click", function() {
+        setTimeout(function() { window.location.reload(); }, 2)
+    })*/
+
+
+    //single picture
+    /*modalPictureCard.addEventListener("click", function() {
+        modalPicture.style.display = "block";
+    });*/
+
+    modalPicture.addEventListener("click", function() {
+        modalPicture.style.display = "block";
+    });
+
+    //close single picture
+    modalCloseDark.addEventListener("click", function() {
+        modalPicture.style.display = "none";
+    });
+}, 500);
 
 const modalPicture = document.getElementById("single-img");
 //const modalPictureCard = document.getElementByClass("photo-card");
 const modalCloseDark = document.getElementById("dark-color-close");
-
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
-function launchModal() {
-    modalbg.style.display = "block";
-}
-
-// close modal event
-modalClose.forEach((btn) => btn.addEventListener("click", closeModal));
-
-//close modal form
-function closeModal() {
-    modalbg.style.display = "none";
-
-}
-
-/*modalSucBtn.addEventListener("click", function() {
-    setTimeout(function() { window.location.reload(); }, 2)
-})
-
-modalSucClose.addEventListener("click", function() {
-    setTimeout(function() { window.location.reload(); }, 2)
-})*/
-
-
-//single picture
-/*modalPictureCard.addEventListener("click", function() {
-    modalPicture.style.display = "block";
-});*/
-
-modalPicture.addEventListener("click", function() {
-    modalPicture.style.display = "block";
-});
-
-//close single picture
-modalCloseDark.addEventListener("click", function() {
-    modalPicture.style.display = "none";
-});
 
 
 function popupPhoto(id, media) {
@@ -83,6 +90,10 @@ function popupPhoto(id, media) {
 }
 
 modalCloseDark.onclick = function() {
+    modalPicture.style.display = "none";
+}
+
+function closeDark() {
     modalPicture.style.display = "none";
 }
 
